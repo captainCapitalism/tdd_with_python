43d1635f423d89b3160c76934626c735ac31a5e9
Michał Bargiel <michal5bargiel@gmail.com>
2018-11-27 12:09:14 +0100

    Use Bootstrap to improve layout

---
+++ todo.py
@@ -0,0 +1,15 @@
+'''
+DONE: Don't save blank items for every request
+DONE: code smell: POST test is to long?
+DONE: dipslay multiple items in the table
+DONE: Clean up after FT runs
+Support more than one list!
+DONE: Remove time.sleeps
+
+DONE: Adjust model so that items are associated with different lists
+DONE: Add unique URLs for each list
+DONE: Add URL for creating a new list via POST
+DONE: URLs for adding a new item to an existing list via POST
+Refactor away some duplication in urls.py
+
+'''

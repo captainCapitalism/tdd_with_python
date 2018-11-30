086a601335f3cbeba52ecdebea77a155ab416bd8
Michał Bargiel <michal5bargiel@gmail.com>
2018-11-30 11:00:26 +0100

    here goes production

--- superlists/settings.py
+++ superlists/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ubq%am9nu72%xhy8!rgygzflm4@#(1rw^5p%mtr0w9^vdgfk81'
+#SECRET_KEY = 'ubq%am9nu72%xhy8!rgygzflm4@#(1rw^5p%mtr0w9^vdgfk81'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
